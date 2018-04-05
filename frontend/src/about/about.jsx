e3af2a0348c78799b584cf87669ddc6ebd544d58
José Lourival Souto Júnior <jsouto@inone.com.br>
2018-04-05 17:20:00 -0300

    termino do frontend sem redux

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>(
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus accusantium praesentium reprehenderit pariatur tempora deserunt sed eveniet cupiditate nemo tenetur, nisi sint laudantium illum magnam provident delectus cum nulla corporis!</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nesciunt consectetur ipsam neque repudiandae, dolorem aliquam vitae quia blanditiis laborum eveniet itaque odio voluptas sunt natus aperiam quos fugit iste.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores unde esse officiis placeat, eum non totam itaque veniam? Aut ullam quod facere nisi nulla temporibus optio tenetur cumque voluptatibus magni!</p>
+    
+    </div>
+)
